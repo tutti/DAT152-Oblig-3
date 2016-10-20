a6bdf40fc597155ad205b4f87da5e59c8eeec6e5
Pål Gjerde <paalvaa@gmail.com>
2016-10-20 16:29:22 +0200

    Innhold på forsiden.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById("bilmeny").addEventListener("click", function(e) {
-            e.preventDefault();
-            if (e.target.tagName == "A" || e.target.tagName == "a") {
-                document.getElementById("hovedvindu").setAttribute("data-klasse", e.target.getAttribute("data-klasse"));
+        let bilmeny = document.getElementById("bilmeny");
+        if (bilmeny) {
+            bilmeny.addEventListener("click", function(e) {
+                e.preventDefault();
+                if (e.target.tagName == "A" || e.target.tagName == "a") {
+                    document.getElementById("hovedvindu").setAttribute("data-klasse", e.target.getAttribute("data-klasse"));
+                }
+            });
+        }
+
+        let toggleMeny = false;
+
+        document.getElementById("hovedmeny-container").addEventListener("click", function(e) {
+            if (e.target.tagName == "DIV") {
+                toggleMeny = !toggleMeny;
+                if (toggleMeny) {
+                    document.getElementById("hovedmeny").setAttribute("class", "active");
+                } else {
+                    document.getElementById("hovedmeny").setAttribute("class", "");
+                }
             }
         });
+
+        let bestillingsfaneKnapp = document.getElementById("bestillingsfane-link");
+        let avbestillingsfaneKnapp = document.getElementById("avbestillingsfane-link");
+        
+        if (bestillingsfaneKnapp) {
+            bestillingsfaneKnapp.addEventListener("click", function(e) {
+                e.preventDefault();
+                bestillingsfaneKnapp.setAttribute("class", "active");
+                avbestillingsfaneKnapp.setAttribute("class", "");
+                document.getElementById("bestillingsfane").setAttribute("class", "active");
+                document.getElementById("avbestillingsfane").setAttribute("class", "");
+            });
+            avbestillingsfaneKnapp.addEventListener("click", function(e) {
+                e.preventDefault();
+                bestillingsfaneKnapp.setAttribute("class", "");
+                avbestillingsfaneKnapp.setAttribute("class", "active");
+                document.getElementById("bestillingsfane").setAttribute("class", "");
+                document.getElementById("avbestillingsfane").setAttribute("class", "active");
+            });
+        }
     });
 })();
